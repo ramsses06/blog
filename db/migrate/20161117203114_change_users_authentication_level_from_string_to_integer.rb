6a672c41ed63b06450cac3b342c1de1ad7167b07
ramsses06 <ramssescorona@gmail.com>
2016-11-22 14:30:30 -0600

    migration change

--- db/migrate/20161117203114_change_users_authentication_level_from_string_to_integer.rb
+++ db/migrate/20161117203114_change_users_authentication_level_from_string_to_integer.rb
@@ -2,7 +2,6 @@ class ChangeUsersAuthenticationLevelFromStringToInteger < ActiveRecord::Migratio
   def change
 
   	remove_column :users, :authentication_level, :string
-  	remove_column :users, :authenticate_level, :string
   	add_column :users, :authentication_level, :integer, default: 1
 
   end
